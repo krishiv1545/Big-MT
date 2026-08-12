3afe5c377a12f95d55ba9b1b91d39054a2484c8b
krishiv1545 <krishiv1545@gmail.com>
2026-08-12 18:56:34 +0530

    Allowed Hosts

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -32,8 +32,10 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = env.list(
+    "ALLOWED_HOSTS",
+    default=["localhost", "127.0.0.1"],
+)
 
 # Application definition
 
